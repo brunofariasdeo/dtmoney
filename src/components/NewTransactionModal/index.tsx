86e8a8ddf2c1390244fe03997b307bb5f58b6f67
Bruno Oliveira <jbfo@cin.ufpe.br>
2021-10-03 22:59:01 -0300

    Create increase/decrease buttons for modal and include Polished dependency

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 
 interface NewTransactionModalProps {
@@ -33,6 +35,20 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
                     placeholder="Valor"
                     type="number"
                 />
+                <TransactionTypeContainer>
+                    <button
+                        type="button"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+                    <button
+                        type="button"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
                 <input
                     placeholder="Categoria"
                 />
