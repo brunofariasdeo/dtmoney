b4d7c0625981a8232d00f647a35fef9181ad629e
Bruno Oliveira <jbfo@cin.ufpe.br>
2021-10-06 23:25:36 -0300

    Close modal after submitting a form and update transaction table component through context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,12 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
+import { TransactionsContext } from '../../TransactionsContext';
+
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { api } from '../../services/api';
-import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -13,23 +14,30 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps){
+    const { createTransaction } = useContext(TransactionsContext);
+
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
     const [title, setTitle] = useState('');
     const [type, setType] = useState('deposit');
-    const [value, setValue] = useState(0);
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = {
+        await createTransaction({
             title,
-            value,
+            amount,
+            category,
             type,
-            category
-        }
+        });
 
-        api.post('/transactions', data)
-    }
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        
+        onRequestClose();
+    };
 
     return (
         <Modal 
@@ -54,10 +62,10 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
                     value={title}
                 />
                 <input
-                    onChange={event => setValue(Number(event.target.value))}
+                    onChange={event => setAmount(Number(event.target.value))}
                     placeholder="Valor"
                     type="number"
-                    value={value}
+                    value={amount}
                 />
                 <TransactionTypeContainer>
                     <RadioBox
